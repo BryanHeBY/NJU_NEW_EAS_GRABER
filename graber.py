29f51da98c0804b76fe2a1d6dc6d0139112c6e79
lrjia <52886379+lrjia@users.noreply.github.com>
2021-03-01 22:12:03 +0800

    增加选择轮次，输出异常，随机等待

--- graber.py
+++ graber.py
@@ -2,53 +2,62 @@ from selenium import webdriver
 import time
 import getpass
 import os
+import random
 
 username = input('请输入用户名')
 password = getpass.getpass('请输入密码')
-gap = float(input('请输入抢课间隔(单位s)'))
 
 '''
 或者直接在这里输入用户名和密码
 username = '这里输入用户名'
 password = '这里输入密码'
 gap = float('这里输入抢课间隔(单位s)')
-
 '''
 
 path = os.getcwd()
 
 def grab():
     #模拟登录
-    #browser = webdriver.Chrome(executable_path=os.path.join(path,'chromedriver.exe'))
-    browser = webdriver.Firefox(executable_path=os.path.join(path,'geckodriver.exe'))
+    browser = webdriver.Chrome(executable_path=os.path.join(path,'chromedriver'))
+    # browser = webdriver.Firefox(executable_path=os.path.join(path,'geckodriver.exe'))
     browser.get("http://xk.nju.edu.cn/xsxkapp/sys/xsxkapp/*default/index.do")
     browser.find_element_by_id("username").send_keys(username)
     browser.find_element_by_id("password").send_keys(password)
-    browser.find_element_by_css_selector("#casLoginForm > p:nth-child(4) > button:nth-child(1)").click()
-    browser.implicitly_wait(10)
+    browser.find_element_by_css_selector("#casLoginForm > p:nth-child(4) > button").click()
+    random_sleep()
+
+    #选择轮次
+    browser.find_element_by_css_selector("body > div.jqx-rc-all.jqx-window.jqx-popup.jqx-widget.jqx-widget-content > div.jqx-window-content.jqx-widget-content.jqx-rc-b > div.content > div > div > table > tbody").click()
+    browser.find_element_by_css_selector("#buttons > button.bh-btn.bh-btn-primary.bh-btn.bh-pull-right").click()
+
     browser.find_element_by_xpath('//*[@id="courseBtn"]').click()
-    time.sleep(1)
+    random_sleep()
 
     #开始抢课
     while (1):
-        browser.implicitly_wait(3)
+        # browser.implicitly_wait(3)
+        random_sleep()
         browser.find_element_by_css_selector("#cvPageHeadTab > li:nth-child(1) > a:nth-child(1)").click()
-        browser.implicitly_wait(3)
+        random_sleep()
         browser.find_element_by_xpath('//a[text()="收藏"]').click()
-        browser.implicitly_wait(3)
+        random_sleep()
         for each in browser.find_elements_by_css_selector('tr.course-tr> td:nth-child(8) > a:nth-child(2)[data-isfull=""]'):
             each.click()
-            time.sleep(0.2)
-            browser.implicitly_wait(3)
+            random_sleep()
             browser.find_element_by_css_selector(".cv-sure").click()
-            time.sleep(1)
+            random_sleep()
             browser.find_element_by_css_selector(".cv-sure").click()
-        time.sleep(gap)
+        random_sleep()
     browser.quit()
+
+def random_sleep():
+    gap = 1 + random.random() * 3
+    time.sleep(gap)
 
 #发现网络异常自动重新载入程序
 while True:
-    try:
-        grab()
-    except Exception:
-        pass
+    while True:
+        try:
+            grab()
+        except Exception as e:
+            print(e)
